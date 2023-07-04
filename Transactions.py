486ee25f1f17645f47062aba6ce12ea3f4e04a95
cdy3870 <thefallen3870@yahoo.com>
2023-07-04 14:29:10 -0400

    updated dashboard

---
+++ Transactions.py
@@ -0,0 +1,173 @@
+import plotly.express as px
+import streamlit as st
+# from pyspark.sql import SparkSession
+# from pyspark.sql.functions import desc, split
+import matplotlib.pyplot as plt
+import plotly.express as px
+# from pyspark.sql.functions import lit
+import functools
+# from pyspark.sql import DataFrame
+import plotly.graph_objects as go
+import pandas as pd
+import pickle
+
+
+
+st.set_page_config(
+	page_title="An Analysis of 100 million+ eCommerce Transactions", layout="wide")
+
+st.markdown("<h1 style='text-align: center'> An Analysis of 100 million+ eCommerce Transactions </h1>", unsafe_allow_html=True)
+
+css='''
+	[data-testid="metric-container"] {
+	    width: fit-content;
+	    margin: auto;
+	}
+
+	[data-testid="metric-container"] > div {
+	    width: fit-content;
+	    margin: auto;
+	}
+
+	[data-testid="metric-container"] label {
+	    width: fit-content;
+	    margin: auto;
+	}
+	'''
+
+
+@st.cache_data
+def get_agg_by_brand():
+	count_by_brand = pd.read_csv("agg_by_brand.csv").dropna().iloc[:14]
+
+	country = ["Korea", "US", "China", "China", "Canada", "Italy", "China", "Korea", "Japan",
+			   "Uzbekistan", "Germany", "Taiwan", "China", "Finland"]
+	count_by_brand["country"] = country
+	# count_by_brand = count_by_brand.set_index("brand")
+	print(list(count_by_brand["brand"]))
+	return count_by_brand
+
+def get_agg_by_brand_plot(agg_by_brand):
+	fig = px.scatter(agg_by_brand,  y="count", color="country",
+					 size="revenue", x="brand", title="Top 15 Brands Based on Purchases", category_orders=dict(group=[agg_by_brand["brand"]]))
+	# fig.update_layout(width=750,height=750)
+
+	return fig
+
+@st.cache_data
+def get_day_stats():
+	day_stats = pd.read_csv("day_stats.csv")
+
+	return day_stats
+
+def get_day_plots(day_stats):
+	fig_1 = px.bar(day_stats, y="day", x=["oct_count", "nov_count"], orientation='h', title=" ")
+	fig_1.update_layout(width=750, height=750, xaxis_title="Number of Purchases")
+
+	fig_2 = px.bar(day_stats, y="day", x=["oct_rev", "nov_rev"], orientation='h', title="Tracking Purchases Throughout the Month", color_discrete_map={
+        'oct_count': 'red',
+        'nov_count': 'green'
+    })
+	fig_2.update_layout(xaxis_title="Total Revenue", xaxis=dict(autorange="reversed"), width=750, height=750, font_color="white")
+	fig_2.update_yaxes(visible=False)
+
+	return fig_1, fig_2
+
+@st.cache_data
+def get_brand_counts():
+	brand_counts = pd.read_csv("brand_counts.csv")
+
+	return brand_counts
+
+def get_brand_counts_plot(brand_counts):
+	fig = px.pie(brand_counts, values='count', names='adjusted_brand', hole=0.3, title="Brand Totals")
+
+	return fig
+
+@st.cache_data
+def get_vtp():
+	vtp = pd.read_csv("v_times.csv")
+
+	return vtp
+
+def get_vtp_plot(vtp):
+	fig = px.scatter(vtp, y=["avg_vtp_time", "avg_vtc_time"], x="brand", title="User Behavior Analysis")	
+
+	return fig
+
+def get_sankey():
+	sankey_df = pd.read_csv("sankey.csv")
+	with open("sankey_labels.txt", "rb") as f:
+		labels = pickle.load(f)
+
+	source = sankey_df["main_cat_index"] 
+	target = sankey_df["subcat_1_index_adjusted"]
+	value = sankey_df["count"]
+	# data to dict, dict to sankey
+	link = dict(source = source, target = target, value = value)
+	node = dict(label = labels, pad=50, thickness=5)
+	data = go.Sankey(link = link, node = node)
+	# plot
+	fig = go.Figure(data)
+	fig.update_layout(
+	    width=750,
+	    height=1000,
+	    title="Breakdown of Product Categories (Purchases)",
+
+	)
+
+	return fig
+
+def main():
+	# spark = SparkSession.builder.appName('test').getOrCreate()
+	# df = process_dfs(spark, ["data/2019-Nov.csv"])
+	# print(df.count())
+	col1, col2, col3, col4, col5 = st.columns(5)
+	col3.metric("Dataset Information", "")
+	col1, col2, col3, col4 = st.columns(4)
+	col1.metric("Time Range", "2 Months")
+	col2.metric("Total Transactions", "110M")
+	col3.metric("Total Brands", "4304")
+	col4.metric("Total Product Categories", "14")
+	col1, col2, col3, col4, col5 = st.columns(5)
+
+	col3.metric("Quick Facts", "")
+	col1, col2, col3, col4 = st.columns(4)
+	col4.metric("Lowest View-to-Purchase Time", "Apple")
+	col1.metric("Most Popular Category", "Electronics")
+	col2.metric("Most Popular Brand (Revenue)", "Apple")
+	col3.metric("Most Popular Brand (Purchases)", "Samsung")
+	st.markdown(f'<style>{css}</style>',unsafe_allow_html=True)
+
+	col_1, col_2, col_3 = st.columns((2, 5, 2), gap="small")
+	
+	temp_df = get_brand_counts()
+
+	plot = get_sankey()
+	col_2.plotly_chart(plot, use_container_width=True)
+
+	col_1, col_2 = st.columns(2)
+	plot = get_brand_counts_plot(temp_df)
+	col_1.plotly_chart(plot, use_container_width=True)
+	temp_df = get_agg_by_brand()
+	plot = get_agg_by_brand_plot(temp_df)
+	col_2.plotly_chart(plot, use_container_width=True)
+
+	temp_df_2 = get_day_stats()
+	count_plot, rev_plot = get_day_plots(temp_df_2)
+	col_1, col_2 = st.columns(2, gap="small")
+	col_1.plotly_chart(rev_plot, use_container_width=True)
+	col_2.plotly_chart(count_plot, use_container_width=True)
+
+
+	col11, col22 = st.columns((2, 7), gap="large")
+	vtp = get_vtp()
+	plot = get_vtp_plot(vtp)
+	col11.markdown("**View to Purchase Time:** The amount of time between a user's viewing of an item to the user's purchase. \
+	\n\n **View to Cart Time:** The amount of time between a user's viewing of an item to the user adding the item to cart.\
+	 \n\n I could only perform this SQL query on 34 million transactions before my ram was used up :/ \
+	 \n\n Therefore I only looked at the top 5 categories.")
+	col22.plotly_chart(plot, use_container_width=True)
+
+if __name__ == "__main__":
+	main()
---
+++ pages/Transactional_Messages.py
@@ -0,0 +1,123 @@
+import plotly.express as px
+import streamlit as st
+# from pyspark.sql import SparkSession
+# from pyspark.sql.functions import desc, split
+import matplotlib.pyplot as plt
+import plotly.express as px
+# from pyspark.sql.functions import lit
+import functools
+# from pyspark.sql import DataFrame
+import plotly.graph_objects as go
+import pandas as pd
+import pickle
+from plotly.subplots import make_subplots
+
+
+
+st.set_page_config(
+	page_title="An Analysis of Transactional Messages", layout="wide")
+
+st.markdown("<h1 style='text-align: center'> An Analysis of 10M+ Transactional Messages </h1>", unsafe_allow_html=True)
+
+@st.cache_data
+def get_dfs():
+	campaigns_df = pd.read_csv("data/campaigns.csv")
+	holidays_df = pd.read_csv("data/holidays.csv")
+	return campaigns_df, holidays_df
+
+def get_frequency():
+	campaigns_df, holidays_df = get_dfs()
+	holidays_df["date"] = pd.to_datetime(holidays_df["date"]).dt.date
+	campaigns_df["started_at"] = pd.to_datetime(campaigns_df["started_at"]).dt.date
+
+	temp_df = campaigns_df.groupby(["started_at", "campaign_type"])[["id"]].count().reset_index().rename({"id":"count"}, axis=1)
+
+	fig_1 = px.area(temp_df, x="started_at", y="count", color="campaign_type")
+
+	with open("dfs/other_msgs.txt", "rb") as f:
+		other_msgs_df = pickle.load(f)
+
+	fig_2 = px.area(other_msgs_df, x="sent_at", y="count", color="message_type")
+
+	return fig_1, fig_2
+
+def get_subject_pies():
+	with open("dfs/subject_pies_missing.txt", "rb") as f:
+		final_dfs = pickle.load(f)
+
+	with open("dfs/subject_pies.txt", "rb") as f:
+		final_dfs_2 = pickle.load(f) 
+
+	combined_dfs = []
+
+	for i in range(4):
+		combined_dfs.append(final_dfs[i])
+		combined_dfs.append(final_dfs_2[i])
+		
+	fig = make_subplots(rows=2, cols=4, specs=[[{"type": "pie"}, {"type": "pie"}, {"type": "pie"}, {"type": "pie"}],
+	 [{"type": "pie"}, {"type": "pie"}, {"type": "pie"}, {"type": "pie"}]], subplot_titles=("Without Personalization", "With Personalization", "Without Discount", "With Discount", "Without Saleout", "With Saleout", "Without Deadline", "With Deadline"))
+
+	col = 1
+	row = 1
+	for i in range(8):
+		fig.add_trace(go.Pie(
+			values=combined_dfs[i]["Count"],
+			labels=combined_dfs[i]["Type"], hole=0.5, 
+			domain=dict(x=[0, 0.5])), 
+			row=row, col=col)
+		col += 1
+		
+		if col == 5:
+			col = 1
+			row += 1  
+
+
+	fig.update_layout(height=600, width=1600, showlegend=False)
+
+	return fig
+
+def get_open_rate_bars():
+	with open("dfs/open_rate_bars.txt", "rb") as f:
+		chart_2_df = pickle.load(f)	
+	fig = px.bar(chart_2_df, x="topic", y="open_rate", color="message_type")
+	fig.update_layout(xaxis={"categoryorder":"total descending"})
+	return fig
+
+def get_sunburst():
+	with open("dfs/open_rate_bars.txt", "rb") as f:
+		chart_2_df = pickle.load(f)	
+	fig = px.sunburst(chart_2_df, path=["message_type", "channel", "topic"], color="message_type", values="total")
+	fig.update_layout(height=700, width=700)
+
+	return fig
+
+def main():
+	st.write("Let's see if messages sent out by companies affect the behavior of users.")
+	st.write("First, let's see how these messages are broken down and how frequently they're sent.")
+	fig_1, fig_2 = get_frequency()
+	st.plotly_chart(get_sunburst(), use_container_width=True)
+	st.plotly_chart(fig_1, use_container_width=True)
+	st.plotly_chart(fig_2, use_container_width=True)
+
+
+
+	st.write("Next, let's see which messages are being opened.")
+	st.plotly_chart(get_open_rate_bars(), use_container_width=True)
+
+	st.write("Looks like most messages being opened are transactional ones. \
+		However, bulk messages are more directly related to the effects of marketing. Do certain keywords in subjects improve the open rate?")
+	sub_col1, sub_col2 = st.columns(2)
+	st.plotly_chart(get_subject_pies(), use_container_width=True)
+
+	st.write("How often do messages lead to purchases?")
+	st.write("Which types of triggers lead to the most purchases?")
+
+	st.write("Which types of messages lead to some adverse action?")
+
+
+
+
+	return None
+
+if __name__ == "__main__":
+	main()
